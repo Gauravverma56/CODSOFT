0ad8c7620bace9319796e6aa9e29febc061f6b46
Gaurav <90822894+Gauravverma56@users.noreply.github.com>
2023-07-17 00:33:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll('button');
+const screen  = document.getElementById('screen');
+
+for (let btn of buttons){
+    btn.addEventListener('click', () =>{
+        const btntext = btn.innerText; 
+        
+        if(btntext === 'C'){
+            screen.value = "";
+        }else if(btntext ==='X'){
+            screen.value += '*'
+        }else if (btntext==='='){
+            try{
+
+                screen.value = eval(screen.value)
+            }
+            catch(er){
+                screen.value= "Invalid value"
+            }
+        }else{
+            screen.value=screen.value+btntext;
+            
+       }
+    })
+}
